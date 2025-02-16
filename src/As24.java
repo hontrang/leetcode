00b0d5aac745ceffe5aae2fe878da8c62799ea5d
hontrang <tang11392@gmail.com>
2025-02-16 13:47:46 +0700

    feat: 24. Swap Nodes in Pairs

---
+++ src/As24.java
@@ -0,0 +1,34 @@
+public class As24 {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode newHead = head.next;
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode nodeX;
+        ListNode nodeY;
+        while ((current != null && current.next != null)) {
+            nodeX = current;
+            nodeY = current.next;
+
+            nodeX.next = nodeY.next;
+            nodeY.next = nodeX;
+
+            if (prev != null) {
+                prev.next = nodeY;
+            }
+
+            prev = nodeX;
+            current = nodeX.next;
+        }
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        As24 obj = new As24();
+        obj.swapPairs(head).printList();
+    }
+}
