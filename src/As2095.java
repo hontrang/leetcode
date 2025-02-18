0e570a521ad949f9c5fb04066527e57181ecf235
hontrang <tang11392@gmail.com>
2025-02-18 11:26:24 +0700

    feat: 2095. Delete the Middle Node of a Linked List

---
+++ src/As2095.java
@@ -0,0 +1,52 @@
+public class As2095 {
+    public ListNode deleteMiddle(ListNode head) {
+        ListNode current = head;
+        ListNode slow = head;
+        ListNode fast = head;
+        if (head == null || head.next == null) {
+            return null;
+        }
+        while (fast != null && fast.next != null) {
+            current = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        current.next = slow.next;
+        return head;
+    }
+
+    public static void main(String[] args) {
+//        ListNode head = new ListNode(7);
+//        head.next = new ListNode(9);
+//        head.next.next = new ListNode(6);
+//        head.next.next.next = new ListNode(6);
+//        head.next.next.next.next = new ListNode(7);
+//        head.next.next.next.next.next = new ListNode(8);
+//        head.next.next.next.next.next.next = new ListNode(3);
+//        head.next.next.next.next.next.next.next = new ListNode(0);
+//        head.next.next.next.next.next.next.next.next = new ListNode(9);
+//        head.next.next.next.next.next.next.next.next.next = new ListNode(5);
+
+//        ListNode head = new ListNode(1);
+//        head.next = new ListNode(2);
+//        head.next.next = new ListNode(3);
+//        head.next.next.next = new ListNode(4);
+//        head.next.next.next.next = new ListNode(5);
+//        head.next.next.next.next.next = new ListNode(6);
+//        As1721 obj = new As1721();
+//        obj.swapNodes(head, 2).printList();
+
+//        ListNode head = new ListNode(1);
+//        head.next = new ListNode(2);
+//        As1721 obj = new As1721();
+//        obj.swapNodes(head, 1).printList();
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        As2095 obj = new As2095();
+        obj.deleteMiddle(head).printList();
+    }
+}
