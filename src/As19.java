b2624a05ab412a6b95c4b2ff70c47a916f36a471
hontrang <tang11392@gmail.com>
2025-02-18 11:38:19 +0700

    feat: 19. Remove Nth Node From End of List

---
+++ src/As19.java
@@ -0,0 +1,54 @@
+public class As19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode current = head;
+        ListNode slow = head;
+        ListNode fast = head;
+        if (head == null || head.next == null) return null;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        if (fast == null) return head.next;
+        while (fast != null) {
+            current = slow;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        current.next = slow.next;
+        return head;
+    }
+
+    public static void main(String[] args) {
+//        ListNode head = new ListNode(7);
+//        head.next = new ListNode(9);
+//        head.next.next = new ListNode(6);
+//        head.next.next.next = new ListNode(6);
+//        head.next.next.next.next = new ListNode(7);
+//        head.next.next.next.next.next = new ListNode(8);
+//        head.next.next.next.next.next.next = new ListNode(3);
+//        head.next.next.next.next.next.next.next = new ListNode(0);
+//        head.next.next.next.next.next.next.next.next = new ListNode(9);
+//        head.next.next.next.next.next.next.next.next.next = new ListNode(5);
+
+//        ListNode head = new ListNode(1);
+//        head.next = new ListNode(2);
+//        head.next.next = new ListNode(3);
+//        head.next.next.next = new ListNode(4);
+//        head.next.next.next.next = new ListNode(5);
+//        head.next.next.next.next.next = new ListNode(6);
+//        As1721 obj = new As1721();
+//        obj.swapNodes(head, 2).printList();
+
+//        ListNode head = new ListNode(1);
+//        head.next = new ListNode(2);
+//        As1721 obj = new As1721();
+//        obj.swapNodes(head, 1).printList();
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        As19 obj = new As19();
+        obj.removeNthFromEnd(head,3).printList();
+    }
+}
